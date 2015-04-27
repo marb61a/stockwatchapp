dec3408d017a7c3d5aea0bf85a95f7bb7752971d
Martin <marb61a@gmail.com>
2015-04-27 11:06:45 +0000

    Fixed folder layout and added service

---
+++ app/assets/javascripts/angular/controllers/stocks_ctrl.js
@@ -0,0 +1,9 @@
+/*app.controller('StocksCtrl', ['$scope', '$resource', function($scope, $resource) {
+  var Stocks = $resource('/api/stocks');
+  $scope.stocks = Stocks.query();
+}]);*/
+
+// Better than above as it allows for easier extension by stock.js modification 
+app.controller('StocksCtrl', ['$scope', 'Stock', function($scope, Stock) {
+  $scope.stocks = Stock.all();
+}]);
--- app/controllers/stocks_ctrl.rb
+++
@@ -1,4 +0,0 @@
-app.controller('StocksCtrl', ['$scope', '$resource', function($scope, $resource) {
-  var Stocks = $resource('/api/stocks');
-  $scope.stocks = Stocks.query();
-}]);
