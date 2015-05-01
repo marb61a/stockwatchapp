4c0c4bc53926a57dd1100cc343eb2db18fb427c4
Martin <marb61a@gmail.com>
2015-05-01 10:50:13 +0000

    finishing crud

--- app/assets/javascripts/angular/controllers/stocks_ctrl.js
+++ app/assets/javascripts/angular/controllers/stocks_ctrl.js
@@ -4,8 +4,16 @@
 }]);*/
 
 // Better than above as it allows for easier extension by stock.js modification 
-app.controller('StocksCtrl', ['$scope', 'Stock', function($scope, Stock) {
+app.controller('StocksCtrl', ['$scope', 'Stock', '$filter', function($scope, Stock, $filter) {
   $scope.stocks = Stock.all();
+  
+  $scope.createStock = function(){
+    var attr = {};
+    attr.symbol = $filter('uppercase')($scope.newSymbol);
+    var newStock = Stock.create(attr);
+    $scope.stocks.push(newStock);
+    $scope.newSymbol = "";
+  };
   
   $scope.deleteStock = function(id, idx) {
     $scope.stocks.splice(idx, 1);
--- app/assets/javascripts/angular/services/stock.js
+++ app/assets/javascripts/angular/services/stock.js
@@ -8,8 +8,14 @@ app.factory('Stock', ['$resource', function($resource) {
   };
   
   Stock.prototype.delete = function(stId) {
-    this.service.remove({stockId: stId});
+    return this.service.remove({stockId: stId});
   };
+  
+  Stock.prototype.create = function(attr){
+    return this.service.save(attr);
+  }
   
   return new Stock;
 }]);
+
+ 
